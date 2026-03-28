a4177a5d03ae6be0176c4aad396f453f02034c3b
kasha2217 <kanikasharma171717@gmail.com>
2026-03-28 07:54:20 +0000

    chore:verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -313,6 +313,9 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 	return grpc.DialContext(ctx, svcAddr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+
+
+
 	)
 }
 
